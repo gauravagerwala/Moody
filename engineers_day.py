caa62f3aa8d7020274f0ae853f8b56182e2f2d85
CoderHam <hemanham@gmail.com>
2015-09-15 01:40:39 +0530

    Engineers Day extras

--- engineers_day.py
+++ engineers_day.py
@@ -4,8 +4,14 @@ import sys
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 video_capture = cv2.VideoCapture(0)
-video_capture .read()
+video_capture.read()
+face_count = 0
+eye_count = 0
+face_no_count = 0
+eye_no_count = 0
 while True:
+    face_there = False
+    eye_there = False
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     img2 = frame
@@ -20,12 +26,33 @@ while True:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         img = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        face_there = True
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img2[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
-    print("Hey there! I see you")
+            eye_there= True
+    if(face_there):
+        face_count = face_count + 1
+        if(face_count>6):
+            print("I see you!")
+            face_count = 0
+    else:
+        face_no_count = face_no_count + 1
+        if(face_no_count>6):
+            print("Where did you go?")
+            face_no_count = 0
+    if(eye_there):
+        eye_count = eye_count + 1
+        if(eye_count>6):
+            print("Your eyes too!")
+            eye_count = 0
+    else:
+        eye_no_count = eye_no_count + 1
+        if(eye_no_count>6):
+            print("Open your eyes!")
+            eye_no_count = 0
     # Display the resulting frame
     cv2.imshow('Video', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
